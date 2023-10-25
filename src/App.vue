69ae948506a29805aa38b8c5174ac374eb4e3664
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-10-25 13:04:25 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import NostrSwitch from "@/components/NostrSwitch.vue";
+import ZapStreamVideo from "@/components/ZapStreamVideo.vue";
+</script>
+
+<template>
+  <main>
+    <h1>NIP-91 Actuator PoC</h1>
+    <p>Send a NIP-91 8000 event and move the Ostwich's head</p>
+    <zap-stream-video></zap-stream-video>
+    <p>Then</p>
+    <nostr-switch></nostr-switch>
+    <p>
+      <a href="https://nostriot.com">nostriot.com</a>
+    </p>
+  </main>
+</template>
+
+<style>
+main {
+  width: 500px;
+  margin: 0 auto;
+}
+
+h1, p {
+  text-align: center;
+}
+</style>
---
+++ src/components/ZapStreamVideo.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <p><a href="https://zap.stream/naddr1qqjr2wr98ycngerp94snvdtz956rgdnp943rxv3j943rgvekve3x2dekxucrwq3qeaz6dwsnvwkha5sn5puwwyxjgy26uusundrm684lg3vw4ma5c2jsxpqqqpmxwwzw3e6"
+        target="_blank"
+        title="Watch the Nostwich">Watch the Nostwich on Zap.Stream</a></p>
+</template>
+
+<style scoped>
+
+</style>
